4e8e3eb5c3f2d073e97141b931bec910830ba799
Kirby Ammari <kirbyammari@gmail.com>
2022-11-16 15:55:41 -0800

    Centered a div woohoo

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.scss'
+
+const HomePage = () => {
+  return (
+    <div className="header">
+        <p className="title">
+            ⌨️typejam.
+        </p>
+    </div>
+  )
+}
+
+export default HomePage
